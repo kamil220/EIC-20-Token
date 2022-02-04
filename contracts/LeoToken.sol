f1e4c94c3d952b41dbc2d25800f4491248c58af3
Kamil Łazarz <kamillazarz@gmail.com>
2022-02-04 18:43:08 +0100

    Setup name, symbol, decimals and total supply

--- contracts/LeoToken.sol
+++ contracts/LeoToken.sol
@@ -5,10 +5,15 @@ import 'hardhat/console.sol';
 
 contract LeoToken {
 
+    string public name = 'Leocode Token';
+    string public symbol = 'LEO';
+    uint8 public decimals = 18;
+
     /* Start Methods */
-    function name() public view returns ( string );
-    function symbol() public view returns ( string );
-    function totalSupply() public view returns ( uint256 );
+    function totalSupply() public view returns ( uint256 ) {
+        return 100_000 * ( 10 ** decimals );
+    }
+
     function balanceOf( address _owner ) public view returns ( uint256 balance );
     function transfer( address _to, uint256 __value ) public returns ( bool success );
     function transferFrom( address _from, address _to, uint256 _value ) public returns ( bool success );
