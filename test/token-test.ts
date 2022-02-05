f1a4010283d25e32e94d974ac6d05ce054122da4
Kamil Łazarz <kamillazarz@gmail.com>
2022-02-05 15:22:35 +0100

    Test for name, symbol, decimals, total amount

--- test/token-test.ts
+++ test/token-test.ts
@@ -1,18 +1,24 @@
 import { expect } from 'chai';
-import { ethers } from 'hardhat';
+import { Contract } from 'ethers';
+import {ethers} from 'hardhat';
 
 describe("LeoToken", function () {
     let owner, notOwner;
-    let token;
+    let token: Contract;
 
     const tokenCount = 100000;
     const zeroAddress = ethers.constants.AddressZero;
     const totalAmount = ethers.constants.WeiPerEther.mul( tokenCount ); // 1 wei has 18 decimals
+    const decimals = 18;
 
     const messages = {
         errorZeroAddress: 'Zero address is not allowed',
         errorLackOfMoney: 'Not enough tokens',
-        errorNotAllowed: 'Not allowed to transfer tokens'
+        errorNotAllowed: 'Not allowed to transfer tokens',
+        checkName: 'The name of token should be `Leocode Token`',
+        checkSymbol: 'The symbol of token should be `LEO`',
+        checkDecimals: 'The token should have 18 decimals',
+        checkTotalAmount: 'The total amount of token is wrong',
     };
 
     beforeEach( async() => {
@@ -21,4 +27,20 @@ describe("LeoToken", function () {
        await token.deployed();
     });
 
+    it( messages.checkName, async() => {
+       expect( await token.name().to.equal( 'Leocode Token' ) );
+    });
+
+    it( messages.checkSymbol, async() => {
+        expect( await token.symbol().to.equal( 'LEO' ) );
+    });
+
+    it( messages.checkDecimals, async() => {
+        expect( await token.decimals().to.equal( decimals ) );
+    })
+
+    it( messages.checkTotalAmount, async() => {
+       const total = await token.totalSupply();
+       expect( total.eq( totalAmount ).to.equal( true ))
+    });
 } );
